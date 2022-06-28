906bf0ed8049a7fe1f0a5e863911093dff0f3ca9
Fernando dos Santos Motta <fernando@Fernandos-MacBook-Pro.local>
2022-06-28 19:29:25 -0300

    Adicionado evento de clique nos botoes e remanejado as pastas style.css e reset.css

---
+++ main.js
@@ -0,0 +1,53 @@
+const botao0 = document.querySelector('#botao-0')
+const botao1 = document.querySelector('#botao-1')
+const botao2 = document.querySelector('#botao-2')
+const botao3 = document.querySelector('#botao-3')
+const botao4 = document.querySelector('#botao-4')
+const botao5 = document.querySelector('#botao-5')
+const botao6 = document.querySelector('#botao-6')
+const botao7 = document.querySelector('#botao-7')
+const botao8 = document.querySelector('#botao-8')
+const botao9 = document.querySelector('#botao-9')
+
+
+var input = document.querySelector('#Exemplo')
+
+botao0.addEventListener("click", () => {
+    input.value += '0'
+})
+
+botao1.addEventListener("click", () => {
+    input.value += '1'
+})
+
+botao2.addEventListener("click", () => {
+    input.value += '2'
+})
+
+botao3.addEventListener("click", () => {
+    input.value += '3'
+})
+
+botao4.addEventListener("click", () => {
+    input.value += '4'
+})
+
+botao5.addEventListener("click", () => {
+    input.value += '5'
+})
+
+botao6.addEventListener("click", () => {
+    input.value += '6'
+})
+
+botao7.addEventListener("click", () => {
+    input.value += '7'
+})
+
+botao8.addEventListener("click", () => {
+    input.value += '8'
+})
+
+botao9.addEventListener("click", () => {
+    input.value += '9'
+})
